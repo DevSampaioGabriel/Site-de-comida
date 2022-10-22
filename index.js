630241fca69e384671b874095851643f6ed6396d
Gabriel Costa Sampaio <102552939+DevSampaioGabriel@users.noreply.github.com>
2022-10-22 11:30:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let botaoMenu = document.querySelector('.menu_icon');
+let Ul = document.getElementById("listaLinks");
+let Ul_links = Ul.children;
+
+botaoMenu.addEventListener('click', () => {
+    let currentDisplay = Ul.style.display
+    if (currentDisplay == "flex") {
+        Ul.style.display = "none"
+    }else {
+        Ul.style.display = "flex"
+        Ul.style.animation = "abrir_menu 2s forwards"
+        Ul.addEventListener("animationend", () => {
+            for (const elemento of Ul_links) {
+                elemento.style.display = "flex"
+            }
+            Ul.removeEventListener("animationend", arguments.callee)
+        })
+    }  
+})
